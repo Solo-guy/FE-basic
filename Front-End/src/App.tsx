c28643da068632b3069f394a791a78fe7b0e9e7a
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-11-08 11:07:29 +0700

    day2

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,13 +1,35 @@
+import NoteItem from "./components/NoteItem";
+
 const App = () => {
   return (
-    <div className="w-[500px] mx-auto bg-gray-200">
-      <h1 className="font-semibold text-2xl text-blue-400">Note Application</h1>
-      <div>
-        <input type="text" className="" placeholder="Title" />
+    <div className="max-w-3xl mx-auto space-y-6">
+      <div className=" space-y-6 bg-white shadow-md rounded p-5">
+        <h1 className="font-semibold text-2xl text-center">Note Application</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Title"
+            className="w-full border-b-2 border-gray-700 outline-none"
+          />
+        </div>
+        <div>
+          <textarea
+            className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
+            placeholder="Description"
+          ></textarea>
+        </div>
+        <div className="text-right">
+          <button className="bg-blue-500 text-white px-5 py-2 rounded">
+            Submit
+          </button>
+        </div>
       </div>
-      <div>
-        <textarea className="" placeholder="Description"></textarea>
-      </div>
+
+      {/* Note items */}
+      <NoteItem title="My first re-usable component" />
+      <NoteItem title="My first re-usable component" />
+      <NoteItem title="My first re-usable component" />
+      <NoteItem title="My first re-usable component" />
     </div>
   );
 };

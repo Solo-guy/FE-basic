58d97ff18403d45b82c12bd8f0cc51787dd7b6f0
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-11-09 12:38:33 +0700

    day 3

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,21 +1,57 @@
+import React, { useState, ChangeEventHandler } from "react";
 import NoteItem from "./components/NoteItem";
+import axios from "axios";
 
+// let title = "";
 const App = () => {
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <div className="max-w-3xl mx-auto space-y-6">
-      <div className=" space-y-6 bg-white shadow-md rounded p-5">
+      <form
+        onSubmit={async (evt) => {
+          evt.preventDefault();
+          const { data } = await axios.post(
+            "http://localhost:8000/note/create",
+            {
+              title: values.title,
+              description: values.description,
+            }
+          );
+          console.log(data);
+        }}
+        className="space-y-6 bg-white shadow-md rounded p-5"
+      >
         <h1 className="font-semibold text-2xl text-center">Note Application</h1>
         <div>
           <input
-            type="text"
             placeholder="Title"
+            type="text"
             className="w-full border-b-2 border-gray-700 outline-none"
+            onChange={handleChange}
+            value={values.title}
+            name="title"
           />
         </div>
         <div>
           <textarea
-            className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
             placeholder="Description"
+            className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
+            value={values.description}
+            onChange={handleChange}
+            name="description"
           ></textarea>
         </div>
         <div className="text-right">
@@ -23,13 +59,13 @@ const App = () => {
             Submit
           </button>
         </div>
-      </div>
+      </form>
 
-      {/* Note items */}
+      {/* Note Items */}
       <NoteItem title="My first re-usable component" />
-      <NoteItem title="My first re-usable component" />
-      <NoteItem title="My first re-usable component" />
-      <NoteItem title="My first re-usable component" />
+      <NoteItem title="Lorem ipsum dolor sit amet consectetur" />
+      <NoteItem title="Hic perferendis at repellendus fuga" />
+      <NoteItem title="In molestiae optio aspernatur repudiandae" />
     </div>
   );
 };
